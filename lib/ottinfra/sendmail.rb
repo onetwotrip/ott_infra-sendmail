fd3cdb5cd340f2c0c2efa867f78b51b463734e8f
Dmitry Shmelev <avikez@gmail.com>
2016-01-29 16:02:25 +0300

    Removed .valid? method

--- lib/ottinfra/sendmail.rb
+++ lib/ottinfra/sendmail.rb
@@ -1,13 +1,5 @@
 require "sendgrid-ruby"
 
-class Hash
-  def has_shape?(shape)
-    all? do |k, v|
-      Hash === v ? v.has_shape?(shape[k]) : shape[k] === v
-    end
-  end
-end
-
 module OttInfra
   class SendMail
     attr_accessor :config
@@ -21,24 +13,14 @@ module OttInfra
     end
 
     def sendmail(email, opts = {})
-      if valid?
-        mail = SendGrid::Mail.new do |m|
-          m.from = @config[:sendgrid_from]
-          m.to = email
-          m.cc = opts[:cc]
-          m.subject = opts[:subject]
-          m.html = opts[:message]
-        end
-        SendGrid::Client.new(sendmail_options).send mail
+      mail = SendGrid::Mail.new do |m|
+        m.from = @config[:sendgrid_from]
+        m.to = email
+        m.cc = opts[:cc]
+        m.subject = opts[:subject]
+        m.html = opts[:message]
       end
-    end
-
-    def valid?
-      @config.has_shape?({
-        :sendgrid_from => String,
-        :sendgrid_user => String,
-        :sendgrid_pass => String
-      })
+      SendGrid::Client.new(sendmail_options).send mail
     end
 
     private
