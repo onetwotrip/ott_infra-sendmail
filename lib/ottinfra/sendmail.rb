1adf835b3e1ef95fda9c140a8cb7f7ae762035ca
Dmitry Shmelev <avikez@gmail.com>
2016-02-05 10:43:40 +0300

    Refactoring sendmail

--- lib/ottinfra/sendmail.rb
+++ lib/ottinfra/sendmail.rb
@@ -2,17 +2,34 @@ require "sendgrid-ruby"
 
 module OttInfra
   class SendMail
-    attr_accessor :config
+    attr_accessor :config, :mails
 
     def initialize( opts = {} )
+      prep_config( opts )
+      self.mails = []
+    end
+
+    def prep_config( opts = {} )
       @config = {
         :sendgrid_from => opts[:from],
         :sendgrid_user => opts[:user],
         :sendgrid_pass => opts[:pass]
       }
+      @client = SendGrid::Client.new(sendmail_options)
     end
 
-    def sendmail( opts = {} )
+    def add( mail )
+      @mails.push mail
+    end
+
+    def sendmail
+      @mails.each do |mail|
+        client_send( mail )
+      end
+    end
+
+    def self.sendmail( opts = {} )
+      prep_config( opts )
       mail = SendGrid::Mail.new do |m|
         m.from = @config[:sendgrid_from]
         m.to = opts[:to]
@@ -20,10 +37,14 @@ module OttInfra
         m.subject = opts[:subject]
         m.html = opts[:message]
       end
-      SendGrid::Client.new(sendmail_options).send mail
+      client_send( mail )
     end
 
     private
+
+    def client_send( mail )
+      @client.send mail
+    end
 
     def sendmail_options
       { api_user: @config[:sendgrid_user], api_key: @config[:sendgrid_pass] }
