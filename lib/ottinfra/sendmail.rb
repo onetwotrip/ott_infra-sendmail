96a4831d218800ae897b98acec5ddec1efb8ec50
Dmitry Shmelev <avikez@gmail.com>
2016-02-05 12:08:06 +0300

    Refactoring

--- lib/ottinfra/sendmail.rb
+++ lib/ottinfra/sendmail.rb
@@ -5,7 +5,7 @@ module OttInfra
     attr_accessor :config, :mails
 
     def initialize( opts = {} )
-      prep_config( opts )
+      prep_config opts
       self.mails = []
     end
 
@@ -17,29 +17,32 @@ module OttInfra
       @client = SendGrid::Client.new(sendmail_options)
     end
 
-    def add( mail )
-      @mails.push mail
+    def add( opts = {} )
+      @mails.push create_mail( opts )
     end
 
     def sendmail
       @mails.each do |mail|
-        client_send( mail )
+        client_send mail
       end
     end
 
     def self.sendmail( opts = {} )
-      prep_config( opts )
-      mail = SendGrid::Mail.new do |m|
+      prep_config opts
+      client_send create_mail( opts )
+    end
+
+    private
+
+    def create_mail( opts = {} )
+      SendGrid::Mail.new do |m|
         m.from = opts[:from]
         m.to = opts[:to]
         m.cc = opts[:cc]
         m.subject = opts[:subject]
         m.html = opts[:message]
       end
-      client_send( mail )
     end
-
-    private
 
     def client_send( mail )
       @client.send mail
